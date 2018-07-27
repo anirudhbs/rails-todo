845f8e6a3029a592199e9d51e67fd84534f2ca00
anirudhbs <anirudhbs96@gmail.com>
2018-07-27 12:18:22 +0530

    Whitelist params, and add show page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,19 @@
 class TodosController < ApplicationController
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    @todo = Todo.new(params[:todo])
+    @todo = Todo.new(todo_params)
     @todo.save
     redirect_to @todo
   end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:title, :text) # whitelist params
+    end
 end
